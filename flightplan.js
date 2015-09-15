124a146670ea01eabd84bf9501938243fea97e91
tomltd <tomfreestone@gmail.com>
2015-09-15 11:51:42 +0000

    added flightplan

--- flightplan.js
+++ flightplan.js
@@ -3,7 +3,7 @@ var plan = require('flightplan');
 // configuration
 plan.target('production', [
   {
-    host: 'tomfreestone.co.uk',
+    host: 'ssh.tomfreestone.co.uk',
     username: 'tomfreestone.co.uk',
     port: 22,
     agent: process.env.SSH_AUTH_SOCK
@@ -19,5 +19,5 @@ plan.local(function(local) {
   local.log('Copy files to remote hosts');
   var filesToCopy = local.exec('git ls-files', {silent: true});
   // rsync files to all the destination's hosts
-  //local.transfer(filesToCopy, '~/www/');
+  local.transfer(filesToCopy, '/www/surfs-up/');
 });
