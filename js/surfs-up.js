e9ca689935de61ec51032a547d627fef0b92d17d
tomltd <tomfreestone@gmail.com>
2016-08-31 10:11:15 +0100

    format the date function

--- js/surfs-up.js
+++ js/surfs-up.js
@@ -52,3 +52,32 @@ for (var i = 0; i < forecast.fadedRating; i++) {
 }
 
 document.getElementById("ratingContainer").innerHTML = rating.join(" ");
+
+
+// Date stuff
+
+function formatDate(date, fmt) {
+    function pad(value) {
+        return (value.toString().length < 2) ? '0' + value : value;
+    }
+    return fmt.replace(/%([a-zA-Z])/g, function (_, fmtCode) {
+        switch (fmtCode) {
+        case 'Y':
+            return date.getUTCFullYear();
+        case 'M':
+            return pad(date.getUTCMonth() + 1);
+        case 'd':
+            return pad(date.getUTCDate());
+        case 'H':
+            return pad(date.getUTCHours());
+        case 'm':
+            return pad(date.getUTCMinutes());
+        case 's':
+            return pad(date.getUTCSeconds());
+        default:
+            throw new Error('Unsupported format code: ' + fmtCode);
+        }
+    });
+}
+
+formatDate(new Date(timestamp), '%H:%m:%s');
