0c391800f535be099ad2603274487a18d45a232c
tomltd <tomfreestone@gmail.com>
2016-09-01 09:56:44 +0100

    add geolocation stub

--- js/surfs-up.js
+++ js/surfs-up.js
@@ -85,4 +85,26 @@ jQuery(document).ready(function($) {
   $('#dates').append('<div id="date">' + jsDate.toDateString() + '</div>' +
     '<div id="month">' + months[jsDate.getMonth()] + '</div>');
 
+  // Geolocation stuff.
+  var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);  
+
 });
